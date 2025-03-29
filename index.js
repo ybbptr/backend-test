bd7f0df533e0252e82bb2d56813852ef5cce67ae
ybbptr <robyrantung25@gmail.com>
2025-03-29 13:52:16 +0700

    fix: add CORS middleware

--- index.js
+++ index.js
@@ -9,15 +9,10 @@ const port = process.env.PORT || 3000;
 
 connectDb();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/comments', require('./routes/commentRouter'));
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  })
-);
 
 app.use(errorHandler);
 
