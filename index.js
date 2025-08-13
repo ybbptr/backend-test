fd7657629c85643fbc70cdee58e6a8aab85e72f4
Roby Rantung <roby.rantung25@gmail.com>
2025-08-13 14:28:54 +0700

    Fix CORS (again)

--- index.js
+++ index.js
@@ -14,12 +14,21 @@ connectDb();
 
 // app.set('trust proxy', 1);
 
-app.use(
-  cors({
-    origin: ['http://localhost:5173', 'https://soilab-app.vercel.app'],
-    credentials: true
-  })
-);
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
