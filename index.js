23e36e80f2f2c3fd2a830f35e8b37b695b4debe8
Roby Rantung <roby.rantung25@gmail.com>
2025-08-14 13:02:00 +0700

    Fix CORS & fix email on employee page

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
   );
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With, Content-Type, Authorization, multipart/form-data'
+    'X-Requested-With, Content-Type, Authorization'
   );
 
   res.setHeader('Access-Control-Allow-Credentials', true);
