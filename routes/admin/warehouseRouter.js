e0de41341e43fee232be0bd9c168936ccd85c171
Roby Rantung <roby.rantung25@gmail.com>
2025-08-15 23:02:21 +0700

    Fix warehouse router to multipart

--- routes/admin/warehouseRouter.js
+++ routes/admin/warehouseRouter.js
@@ -10,14 +10,28 @@ const {
   getWarehouse
 } = require('../../controller/admin/warehouseController');
 const validate = require('../../middleware/validations/validate');
+const throwError = require('../../middleware/errorHandler');
 const validateWarehouse = require('../../middleware/validations/validateWarehouse');
+const multer = require('multer');
+const upload = multer();
 
 Router.post(
   '/add-warehouse',
+  upload.none(),
+  (req, res, next) => {
+    if (req.body.shelves) {
+      try {
+        req.body.shelves = JSON.parse(req.body.shelves);
+      } catch (err) {
+        return throwError('Format shelves harus berupa JSON valid', 400);
+      }
+    }
+    next();
+  },
   validate(validateWarehouse),
   checkDuplicate(Warehouse, { warehouse_code: 'Kode gudang' }),
   addWarehouse
-).get('/all-warehouse', getWarehouses);
+);
 
 Router.put('/update/:id', validate(validateWarehouse), updateWarehouse)
   .delete('/remove/:id', removeWarehouse)
