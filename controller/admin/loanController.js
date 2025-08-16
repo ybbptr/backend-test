91350189d8faa5fc232f72c78c7ad4805d7fc08d
Roby Rantung <roby.rantung25@gmail.com>
2025-08-16 13:35:19 +0700

    Fix delete feature

--- controller/admin/loanController.js
+++ controller/admin/loanController.js
@@ -111,7 +111,7 @@ const removeLoan = asyncHandler(async (req, res) => {
 
     if (loan_item.approval === 'Disetujui') {
       item.quantity += loan_item.loan_quantity;
-      item.loan_quantity = loan_quantity;
+      item.loan_quantity = loan_item.loan_quantity;
 
       await item.save({ session });
     }
